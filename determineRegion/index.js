780e4485d324b53c09c126fd42d7efeb3574e7aa
Eric Zanchi <ezanchi@gsandf.com>
2018-01-31 23:38:51 -0600

    Added example/documentation

--- determineRegion/index.js
+++ determineRegion/index.js
@@ -1,3 +1,21 @@
+/**
+ * Finds which region a point, p2, lies in, given a center point, p1, and a list
+ * of regions
+ *              ○ p2
+ *  0 rads │   ╱
+ * ┌───────┼───────┐  Example:
+ * │   0   │ ╱  3  │  determineRegion(p1, p2, getRegions(4));
+ * │     p1│╱      │  ===
+ * ├───────○───────│  {  // These are in radians
+ * │   1   │   2   │     min: 4.71238898038469,  // (3/2π rads)
+ * │       │       │     max: 6.283185307179586, // (2π rads)
+ * └───────┼───────┘     region: 3
+ *         │ π rads    }
+ * @param {[number, number]} p1 The center point
+ * @param {[number, number]} p2 The point to find which region it belongs in
+ * @param {regions[]} regions A list of angle regions
+ * @returns {region}
+ */
 const determineRegion = (p1, p2, regions) => {
   const width = p2[0] - p1[0];
   const height = p2[1] - p1[1];
